42847202416e33a63df7d8121db60d9411325807
Cory Pendergraft <cory.pendergraft81@gmail.com>
2022-12-07 19:29:22 -0500

    added correct number of time slots

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+var currDate = $('#currentDay');
+        currDate.text(new Date());
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
