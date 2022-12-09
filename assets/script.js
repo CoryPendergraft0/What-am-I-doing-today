e6e6a34dec3c379b929ebe52f24c89597931c0d5
Cory Pendergraft <cory.pendergraft81@gmail.com>
2022-12-08 21:09:22 -0500

    i finally got it to stay on the page after refreshing

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,31 @@
-var currDate = $('#currentDay');
-        currDate.text(new Date());
+$(function planner() {
 
+  
+  
   var saveBtn = $(".saveBtn")
-
-
-$(function timeColor() {
   var currentHour = dayjs().hour();
-
-    $(".description").each(function() {
-        var currHour = parseInt($(this).parent().attr("id").replace("hour-", ""));
-        
-        if (currHour < currentHour) {
-            $(this).addClass("past");
-        } else if (currHour === currentHour) {
-            $(this).addClass("present");
-        } else {
-            $(this).addClass("future");
-        }
-    })
-});
-
-$(saveBtn).click(function() {
-  var textInput = $(this).siblings(".description").val();time = $(this).parent().attr("id").split("-")[1];
-  localStorage.setItem(textInput, time);
+  
+  $(saveBtn).click(function() {
+    var textInput = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, textInput);
+  })
+  
+  $(".description").each(function() {
+    var currHour = parseInt($(this).parent().attr("id").replace("hour-", ""));
+    
+    if (currHour < currentHour) {
+      $(this).addClass("past");
+    } else if (currHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+    
+    var text = localStorage.getItem($(this).parent().attr("id"));
+    $(this).val(text);
+  });
+  var currDate = $('#currentDay');
+  currDate.text(new Date());
+  
 })
